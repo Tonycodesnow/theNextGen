608b7257bcae046cdd57efdbc1ddd437d3b6b74a
joseduardo15062005 <joseduardo15062005@gmail.com>
2021-11-04 19:25:35 -0700

    add button layout

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,9 +5,19 @@ router.get("/", (req, res) => {
   res.render("home");
 });
 
+//GET login route page
+router.get("/login", (req, res) => {
+  res.render("login", { layout: "login" });
+});
+
 //GET Privacy Policy
 router.get("/privacy-policy", (req, res) => {
   res.render("privacy-policy");
+});
+
+//Get icons page
+router.get("/icons", (req, res) => {
+  res.render("icons");
 });
 
 module.exports = router;
