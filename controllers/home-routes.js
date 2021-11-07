0cb0c4175bc51ba895d1fd2e058b1b82547b91ad
joseduardo15062005 <joseduardo15062005@gmail.com>
2021-11-07 11:10:37 -0800

    add routes for dashboard

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -25,4 +25,6 @@ router.get("/owner", (req, res) => {
   res.render("owner");
 });
 
+
+
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes");
+const dashboardRoutes = require("./dashboard-routes");
 
 router.use("/", homeRoutes);
+router.use("/dashboard", dashboardRoutes);
 router.use("/api", apiRoutes);
 
 router.use((req, res) => {
