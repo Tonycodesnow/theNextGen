0b5aa3cf22d23c9a6e352a4a635a76413aadf464
Sivan Kaplan <sivanagar@gmail.com>
2021-11-08 08:23:13 -0800

    add participats

--- public/js/invite.js
+++ public/js/invite.js
@@ -1,8 +1,34 @@
-document.getElementById("id").addEventListener("click", addParticipant);
+let inputId = 3;
+
+document.getElementById("add").addEventListener("click", addParticipant);
 
 
 
 function addParticipant() {
-  // const inputEl = document.getElementById("input-participant");
-
+  event.preventDefault();
+  const inputEl = document.getElementById("input-participant");
+  
+  newInput=`<div>Participant #${inputId}</div>
+  <div class="form-floating mb-3">
+    <input
+      type="text"
+      class="form-control my-input"
+      id="name${inputId}"
+      placeholder="Name"
+      required
+    />
+    <label for="name${inputId}">Name</label>
+  </div>
+  <div class="form-floating mb-3">
+    <input
+      type="email"
+      class="form-control my-input"
+      id="email${inputId}"
+      placeholder="name@example.com"
+      required
+    />
+    <label for="email${inputId}">Email address</label>
+  </div>`
+  inputEl.innerHTML += newInput;
+  inputId++;
 }
